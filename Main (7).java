9bd9f38ea95bfd73696a56e2fc85700d60200eac
RamshaRauf <113073103+RamshaRauf@users.noreply.github.com>
2022-10-04 15:33:07 -0400

    Runs the program.

---
+++ Main (7).java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) {
+
+    // DecisionTree tree = new DecisionTree("Is it a mammal? ");
+    
+    // tree.setLeft(new DecisionTree("Is it a small mammal? "));
+    // tree.getLeft().setLeft(new DecisionTree("mouse"));
+    // tree.getLeft().setRight(new DecisionTree("bear"));
+    
+    // tree.setRight(new DecisionTree("Is it a reptile? "));
+    // tree.getRight().setLeft(new DecisionTree("snake"));
+    // tree.getRight().setRight(new DecisionTree("fish"));
+
+    
+    // // System.out.println(tree.followPath(""));
+   
+
+
+
+  
+    // try{
+    //   tree.printTree("AnimalTreeTest.txt",tree);
+    // }catch(IOException ioe){
+    //   System.out.println("Exception");
+    // }
+    
+
+
+    //System.out.println(tree.followPath("YN"));
+
+  
+  }
+}
